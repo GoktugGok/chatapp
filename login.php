da14900b8a18dba043f081d41138f62e4ed91035
GoktugGok <gokgoktug0@gmail.com>
2023-02-22 17:09:53 +0300

    first commit

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+    session_start();
+    if (isset($_SESSION['unique_id'])) {
+        header("location: users.php");
+    }
+
+    include_once "header.php";
+?>
+  <body>
+    <div class="wrapper">
+        <section class="form login">
+            <header>Realtime Chat App</header>
+            <form action="#" autocomplete="off">
+                <div class="error-txt">This is an error message!</div>
+                    <div class="field input">
+                        <label for="">Email Address</label>
+                        <input type="text" name="email" placeholder="Enter your email">
+                    </div>
+                    <div class="field input">
+                        <label for="">Password</label>
+                        <input type="password" name="password" placeholder="Enter new password">
+                        <i class="fas fa-eye"></i>
+                    </div>
+                    <div class="field button">
+                        <input type="submit" value="Continue to Chat">
+                    </div>
+            </form>
+            <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+        </section>
+    </div>
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/login.js"></script>
+
+
+  </body>
+</html>
